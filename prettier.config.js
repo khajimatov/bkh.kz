a2b7dcb85560aee556ccd0cfb18bec35b607906b
Bkh <for.official.bkh@gmail.com>
2024-01-24 11:50:29 +0600

    wip

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
